0xClandestine/metaltile

refactor(ops): migrate core/elementwise primitives into kernels/ops/
Completes wave 1: moves the core/elementwise primitive kernels out of the
legacy mlx/ + ffai/ split into kernels/ops/ per the consolidation plan.

- mlx/{arange,arg_reduce,binary,binary_two,copy,gather_axis,hadamard,
  hadamard_m,indexing,logsumexp,random,reduce,scan,scatter_axis,strided,
  ternary,unary} -> kernels/ops/ (already mt_*; inventory-registered, so no
  import sites to update beyond one snapshot test)
- ffai/{axpy_scalar_inplace,clamp_scalar,gather} + mt_vector_add.rs (renamed
  vector_add.rs) -> kernels/ops/ with ffai_ -> mt_ prefixes
- delete ffai/arg_reduce.rs: ffai_argmax was a byte-for-byte duplicate of
  mt_argmax (verified) -> real dedup
- fence.rs moves but stays intentionally undeclared (device/system fences have
  no DSL representation — the '1 out of scope' op)

Updated the steel_msl_snapshots import, the op_group/name-filter test fixtures,
the plan (ops done, wave 1 complete) and KERNEL_AUDIT rows.

cargo build + workspace clippy clean; all ops kernel tests + steel_msl_snapshots
pass.

crates/metaltile-core/src/protocol.rs
@@ -323,7 +323,7 @@ mod tests {
         assert_eq!(op_group("mt_softmax_f32"), "softmax");
         assert_eq!(op_group("mt_softmax_bf16"), "softmax");
         assert_eq!(op_group("softmax"), "softmax");
-        assert_eq!(op_group("ffai_vector_add_f16"), "ffai_vector_add");
+        assert_eq!(op_group("mt_vector_add_f16"), "mt_vector_add");
     }
 
     #[test]

giveen/iron

crates/metaltile-core/src/protocol.rs
@@ -323,7 +323,7 @@ mod tests {
         assert_eq!(op_group("mt_softmax_f32"), "softmax");
         assert_eq!(op_group("mt_softmax_bf16"), "softmax");
         assert_eq!(op_group("softmax"), "softmax");
-        assert_eq!(op_group("ffai_vector_add_f16"), "ffai_vector_add");
+        assert_eq!(op_group("mt_vector_add_f16"), "vector_add");
     }
 
     #[test]